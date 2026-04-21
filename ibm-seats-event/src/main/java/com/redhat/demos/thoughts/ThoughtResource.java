5a1a3ee05977e1fe298dbf7648efcdb760d7d240
jeremyrdavis <jeremyrdavis@mac.com>
2026-04-21 13:22:02 -0400

    added UI

--- ibm-seats-event/src/main/java/com/redhat/demos/thoughts/ThoughtResource.java
+++ ibm-seats-event/src/main/java/com/redhat/demos/thoughts/ThoughtResource.java
@@ -1,16 +1,24 @@
 package com.redhat.demos.thoughts;
 
 import io.quarkus.logging.Log;
+import io.quarkus.qute.CheckedTemplate;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import java.util.List;
+import java.util.Random;
 
 @Path("/api/thoughts")
 public class ThoughtResource {
 
     @POST
+    @Transactional
+    @Produces(MediaType.APPLICATION_JSON)
     public Thought addThought(Thought thought) {
         Log.debug("Adding thought: " + thought);
         thought.persist();
@@ -19,8 +27,10 @@ public class ThoughtResource {
     }
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Thought> getThoughts() {
         Log.debug("Retrieving all thoughts");
         return Thought.listAll();
     }
+
 }
