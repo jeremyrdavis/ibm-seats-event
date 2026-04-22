4eb4b44a4619e23ffb6c7c402237f46b5692a959
jeremyrdavis <jeremyrdavis@mac.com>
2026-04-22 04:30:00 -0400

    round tripping

--- thoughts-evaluator/src/main/java/com/redhat/demos/MyMessagingApplication.java
+++ thoughts-evaluator/src/main/java/com/redhat/demos/MyMessagingApplication.java
@@ -2,6 +2,7 @@ package com.redhat.demos;
 
 import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.control.ActivateRequestContext;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
@@ -17,17 +18,18 @@ public class MyMessagingApplication {
     ThoughtEvaluator evaluator;
 
     @Incoming("thoughts-in")
+    @ActivateRequestContext
     public void evaluate(Thought thought) {
         Log.debugf("Received thought: %s", thought);
         String verdict = evaluator.evaluate(thought.content());
         Log.infof("Thought evaluated as: %s", verdict);
-        if ("APPROVED".equalsIgnoreCase(verdict.strip())) {
-            Log.debugf("Thought has been approved. Sending to output channel: %s", thought);
-            emitter.send(new ThoughtEvaluation(thought.id(), true));
-        } else {
-            Log.infof("Thought REJECTED: %s", thought.content());
-            emitter.send(new ThoughtEvaluation(thought.id(), false));
-        }
+//        if ("APPROVED".equalsIgnoreCase(verdict.strip())) {
+//            Log.debugf("Thought has been approved. Sending to output channel: %s", thought);
+//            emitter.send(new ThoughtEvaluation(thought.id(), true));
+//        } else {
+//            Log.infof("Thought REJECTED: %s", thought.content());
+//            emitter.send(new ThoughtEvaluation(thought.id(), false));
+//        }
     }
 
 }
